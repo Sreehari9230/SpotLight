4cc99c242cc59445fa9944f999946fbb64d27931
Sreehari <sreeharimangalasseri528@gmail.com>
2026-03-16 18:59:06 +0530

    genrePrblm-solved

--- backend/services/tmdb.service.js
+++ backend/services/tmdb.service.js
@@ -3,12 +3,15 @@ import { TMDB_BASE_URL, TMDB_HEADERS } from "../config/tmdb.js";
 
 export const fetchMoviesFromTMDB = async (filters) => {
     const {
-        genre,
+        genres,
         language,
         rating,
         yearFrom,
         yearTo,
     } = filters;
+
+
+    console.log(genres, 'genre in line 14')
 
 const params = {
   include_adult: false,
@@ -32,7 +35,7 @@ const params = {
 };
 
     // Handle "any"
-    if (genre !== "any") params.with_genres = genre;
+    if (genres !== "any") params.with_genres = genres;
     if (language !== "any") params.with_original_language = language;
 
     // console.log(
@@ -60,6 +63,9 @@ const params = {
 
     // 2️⃣ Pick random page
     const randomPage = Math.floor(Math.random() * totalPages) + 1;
+
+    console.log(params, 'params in movie service');
+    
 
     // 3️⃣ Fetch random page
     const randomRes = await axios.get(
