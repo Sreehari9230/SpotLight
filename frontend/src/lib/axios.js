58f0bc3ce06f03991a62ebda0cab60f2fd3dba91
Sreehari <sreeharimangalasseri528@gmail.com>
2026-03-24 13:18:09 +0530

    baseUrl

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://spotlight-d9vi.onrender.com",
 });
 
 // https://spotlight-d9vi.onrender.com
